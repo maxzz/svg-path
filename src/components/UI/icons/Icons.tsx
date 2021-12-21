733a9dfd984c5d5d1b0c79b225736127b7a2bbec
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-21 01:28:45 -0800

    WIP. app skeleton. add menu icon

--- src/components/UI/icons/Icons.tsx
+++ src/components/UI/icons/Icons.tsx
@@ -9,3 +9,14 @@ export function IconChevronDown(props: SVGProps<SVGSVGElement> & { title?: strin
         </svg>
     );
 }
+
+
+export function IconMenu(props: SVGProps<SVGSVGElement> & { title?: string; }) {
+    const { title, ...rest } = props;
+    return (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" />
+        </svg>
+    );
+}
