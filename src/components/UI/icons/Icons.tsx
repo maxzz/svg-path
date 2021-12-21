c89294f1c41ed94a13861a43d6db70ded19220bf
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-20 17:37:18 -0800

    WIP. app skeleton. add icons

---
+++ src/components/UI/icons/Icons.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function MdiChevronDown(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z" fill="currentColor">
+            </path>
+        </svg>
+    );
+}
