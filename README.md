d1ce0e18b9255782f9ec7ca7e73eb2b666ee07af
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-20 17:09:27 -0800

    WIP. app skeleton. add initial panels

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ SVG path exploration
 #### Credits, refs, links
 
 WIP
+
+[svg-path-editor](https://github.com/Yqnn/svg-path-editor)
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
 
+function PathPane() {
+    return (
+        <div className="">pane</div>
+    );
+}
+
+function PathOptions() {
+    return (
+        <div className="">options</div>
+    );
+}
+
+function PathOperations() {
+    return (
+        <div className="">operations</div>
+    );
+}
+
+function PathCommands() {
+    return (
+        <div className="">commands</div>
+    );
+}
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <p className='text-red-700 font-black'>Ground zero</p>
+                <PathOptions />
+                <PathOptions />
+                <PathOperations />
+                <PathCommands />
             </header>
         </div>
     );
