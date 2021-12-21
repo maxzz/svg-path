d318cfb39e6ec8f0f8ad9e03aa55d0f15d01e932
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-20 17:42:21 -0800

    WIP. app skeleton. add title prop to icon props

--- src/components/UI/icons/Icons.tsx
+++ src/components/UI/icons/Icons.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { SVGProps } from "react";
 
-export function MdiChevronDown(props: React.SVGProps<SVGSVGElement>) {
+export function MdiChevronDown(props: SVGProps<SVGSVGElement> & { title?: string; }) {
+    const {title, ...rest} = props;
     return (
-        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
             <path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z" fill="currentColor">
             </path>
         </svg>
