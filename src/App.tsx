3d99e8f2d6f8f6dc92f194f5dffbe0efe5f1d3e3
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-20 18:08:19 -0800

    WIP. app skeleton. add header collapse/expand

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { atom, useAtom } from 'jotai';
+import React, { HTMLAttributes, ReactNode, useState } from 'react';
 import './App.css';
 import { IconChevronDown } from './components/UI/icons/Icons';
 
-function SectionPane({ children }: { children?: React.ReactNode; }) {
+function SectionPane({ children, ...rest }: { children?: ReactNode; } & HTMLAttributes<HTMLDivElement> ) {
     return (
-        <div className="px-2 py-1 bg-slate-500 text-stone-100 uppercase flex items-center justify-between" style={{fontFamily: "Yanone"}}>
+        <div className="px-2 py-1 bg-slate-500 text-stone-100 uppercase flex items-center justify-between" style={{fontFamily: "Yanone"}} {...rest}>
             <div className="pt-1">{children}</div>
             <div className="pr-1">
                 <IconChevronDown className="w-6 h-6" />
@@ -14,49 +15,51 @@ function SectionPane({ children }: { children?: React.ReactNode; }) {
 }
 
 function PanelPath() {
+    const [openAtom] = useState(atom(true));
+    const [open, setOpen] = useAtom(openAtom);
     return (
         <div className="">
-            <SectionPane>Path</SectionPane>
-            path edit
+            <SectionPane onClick={() => setOpen(v => !v)}>Path</SectionPane>
+            {open && <div className="">path edit</div>}
         </div>
     );
 }
 
-function PanelOptions() {
-    return (
-        <div className="">
-            <SectionPane>options</SectionPane>
-            configuration
-        </div>
-    );
-}
+// function PanelOptions() {
+//     return (
+//         <div className="">
+//             <SectionPane>options</SectionPane>
+//             configuration
+//         </div>
+//     );
+// }
 
-function PanelOperations() {
-    return (
-        <div className="">
-            <SectionPane>Operations</SectionPane>
-            scale, translate, round ...
-        </div>
-    );
-}
+// function PanelOperations() {
+//     return (
+//         <div className="">
+//             <SectionPane>Operations</SectionPane>
+//             scale, translate, round ...
+//         </div>
+//     );
+// }
 
-function PanelCommands() {
-    return (
-        <div className="">
-            <SectionPane>Path commands</SectionPane>
-            Commands
-        </div>
-    );
-}
+// function PanelCommands() {
+//     return (
+//         <div className="">
+//             <SectionPane>Path commands</SectionPane>
+//             Commands
+//         </div>
+//     );
+// }
 
 function PathPane() {
     return (
         <div className="w-[300px] max-w-[300px] bg-slate-100 border">
             <p className='text-red-700 font-black'>Ground zero</p>
             <PanelPath />
-            <PanelOptions />
+            {/* <PanelOptions />
             <PanelOperations />
-            <PanelCommands />
+            <PanelCommands /> */}
         </div>
     );
 }
