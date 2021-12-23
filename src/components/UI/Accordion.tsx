f9189435144424c70eb696510d744c2b77239deb
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-22 18:52:08 -0800

    Fix Accordion component first run animation

--- src/components/UI/Accordion.tsx
+++ src/components/UI/Accordion.tsx
@@ -4,10 +4,12 @@ import { a, config, useSpring } from '@react-spring/web';
 
 export function Accordion({ toggle, children }: { toggle: boolean, children: React.ReactNode; }) {
     const [ref, { height, top }] = useMeasure<HTMLDivElement>();
+    const [firstRun, setFirstRun] = React.useState(true);
     const animation = useSpring({
         overflow: "hidden",
         height: toggle ? height + top : 0,
-        config: {mass: 0.2, tension: 392, clamp: true },
+        config: firstRun ? { duration: 0 } : { mass: 0.2, tension: 392, clamp: true },
+        onRest: () => firstRun && setFirstRun(false),
     });
     return (
         <div>
