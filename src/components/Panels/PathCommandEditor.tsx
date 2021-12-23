aa1c09eeb3e0ce4bc20993b2bb0ca3c9f7ca7599
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-23 02:06:18 -0800

    WIP. svg core. add PathCommandEditor edit atoms

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -1,6 +1,7 @@
-import { useAtom } from "jotai";
-import React from "react";
+import { atom, PrimitiveAtom, useAtom } from "jotai";
+import React, { useEffect } from "react";
 import { svgAtom } from "../../store/store";
+import { SvgItem } from "../../svg/svg";
 import { IconMenu } from "../UI/icons/Icons";
 
 function CommandName({ command, abs }: { command: string; abs: boolean; }) {
@@ -12,12 +13,31 @@ function CommandName({ command, abs }: { command: string; abs: boolean; }) {
     );
 }
 
-function CommandInput() {
+function CommandInput({ atom }: { atom: PrimitiveAtom<number>; }) {
     return (
         <label className={`flex-1 max-w-[2rem] rounded-tl-sm overflow-hidden focus-within:text-blue-500`}>
-            <input className="px-1 pb-[1px] w-full h-full text-xs text-center text-slate-900 focus:border-blue-500 bg-slate-200 focus:outline-none" defaultValue={"11"} />
+            <input
+                className="px-1 pb-[1px] w-full h-full text-xs text-center text-slate-900 focus:border-blue-500 bg-slate-200 focus:outline-none"
+                defaultValue={"11"}
+            />
         </label>
     );
+}
+
+function OneCommand({ path }: { path: SvgItem; }) {
+    const createAtoms = () => Array.from({ length: path.values.length }).map((_, idx) => atom(path.values[idx]));
+
+    const [valuesAtoms, setValuesAtoms] = React.useState(createAtoms());
+    useEffect(() => {
+        setValuesAtoms(createAtoms());
+    }, [path]);
+
+    return (<>
+        <CommandName command={path.getType()} abs={true} />
+        {path.values.map((value, idx) => (
+            <CommandInput atom={valuesAtoms[idx]} key={idx} />
+        ))}
+    </>);
 }
 
 export function PathCommandEditor() {
@@ -27,7 +47,7 @@ export function PathCommandEditor() {
     return (
         <div className="my-1">
             {/* Row */}
-            <div className="flex items-center justify-between">
+            {/* <div className="flex items-center justify-between">
                 <div className="flex items-center justify-items-start space-x-0.5">
                     <CommandName command="m" abs={true} />
                     <CommandInput />
@@ -40,41 +60,14 @@ export function PathCommandEditor() {
                 <button className="flex-0 px-1 mt-0.5 active:scale-[.97]">
                     <IconMenu className="w-4 h-4" />
                 </button>
-            </div>
-            {/* Row */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center justify-items-start space-x-0.5">
-                    <CommandName command="m" abs={true} />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                </div>
-                <button className="flex-0 px-1 mt-0.5 active:scale-[.97]">
-                    <IconMenu className="w-4 h-4" />
-                </button>
-            </div>
-            {/* Row */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center justify-items-start space-x-0.5">
-                    <CommandName command="m" abs={true} />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                    <CommandInput />
-                </div>
-                <button className="flex-0 px-1 mt-0.5 active:scale-[.97]">
-                    <IconMenu className="w-4 h-4" />
-                </button>
-            </div>
+            </div> */}
             {svg.path.map((path, idx) => (
                 <React.Fragment key={idx}>
                     <div className="">{path.asString()}</div>
                 </React.Fragment>
+            ))}
+            {svg.path.map((path, idx) => (
+                <OneCommand path={path} key={idx} />
             ))}
         </div>
     );
