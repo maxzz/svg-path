a0d2dd9eebced427f43a31fb4195d3e494052246
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-24 15:14:41 -0800

    WIP. Canvas. add viewPort calculations

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,6 +2,7 @@ import { useAtom } from 'jotai';
 import React, { SVGProps } from 'react';
 import { useMeasure } from 'react-use';
 import { svgAtom } from '../store/store';
+import { SvgPoint } from '../svg/svg';
 
 function GridPattern() {
     return (
@@ -39,14 +40,47 @@ function GridPattern() {
     );
 }
 
+function getPointsBoundingBox(targetPoints: SvgPoint[]): { xmin: number; ymin: number; xmax: number; ymax: number; } {
+    let xmin = 0;
+    let ymin = 0;
+    let xmax = 10;
+    let ymax = 10;
+    if (targetPoints.length) {
+        xmin = Math.min(...targetPoints.map(pt => pt.x));
+        ymin = Math.min(...targetPoints.map(pt => pt.y));
+        xmax = Math.max(...targetPoints.map(pt => pt.x));
+        ymax = Math.max(...targetPoints.map(pt => pt.y));
+    }
+    return { xmin, ymin, xmax, ymax, };
+}
+
+function getViewPort(canvasWidth: number, canvasHeight: number, targetPoints: SvgPoint[]): { x: number; y: number; w: number; h: number; } {
+
+    const box = getPointsBoundingBox(targetPoints);
+
+    let w = box.xmax - box.xmin + 2;
+    let h = box.ymax - box.ymin + 2;
+
+    const ratio = canvasHeight / canvasWidth;
+    if (ratio < h / w) {
+        w = h / ratio;
+    } else {
+        h = ratio * w;
+    }
+
+    return { x: box.xmin - 1, y: box.ymin - 1, w, h, };
+}
+
 function Canvas() {
     const [svg] = useAtom(svgAtom);
-    const [ref, { width, height }] =  useMeasure<HTMLDivElement>();
+    const [ref, { width, height }] = useMeasure<HTMLDivElement>();
     //console.log({ width, height });
-    
+
+    //const viewPort = 
+
     return (
         <div ref={ref} className="absolute w-full h-full -z-10">
-        <svg viewBox="0 0 200 100">
+            <svg viewBox="0 0 200 100">
                 <GridPattern />
                 <rect width="100%" height="100%" fill="#040d1c" /> {/* #002846 */}
                 <rect width="100%" height="100%" fill="url(#c)" />
