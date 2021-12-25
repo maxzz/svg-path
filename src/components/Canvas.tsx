8144cf77e005556414abc554baaf6dd7010436ce
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-24 16:01:38 -0800

    WIP. Canvas. fix SVG pattern IDs

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,7 +7,7 @@ import { getViewPort } from '../svg/svg-utils';
 function GridPattern() {
     return (
         <defs>
-            <pattern id="a" width={10} height={10} patternUnits="userSpaceOnUse">
+            <pattern id="grid-patt-a" width={10} height={10} patternUnits="userSpaceOnUse">
                 <path
                     d="M10 0H0v10"
                     fill="none"
@@ -16,8 +16,8 @@ function GridPattern() {
                     strokeOpacity={0.5}
                 />
             </pattern>
-            <pattern id="b" width={100} height={100} patternUnits="userSpaceOnUse">
-                <path fill="url(#a)" d="M0 0h100v100H0z" />
+            <pattern id="grid-patt-b" width={100} height={100} patternUnits="userSpaceOnUse">
+                <path fill="url(#grid-patt-a)" d="M0 0h100v100H0z" />
                 <path
                     d="M100 0H0v100"
                     fill="none"
@@ -26,8 +26,8 @@ function GridPattern() {
                     strokeOpacity={0.2}
                 />
             </pattern>
-            <pattern id="c" width={200} height={200} patternUnits="userSpaceOnUse">
-                <path fill="url(#b)" d="M0 0h200v200H0z" />
+            <pattern id="grid-patt-c" width={200} height={200} patternUnits="userSpaceOnUse">
+                <path fill="url(#grid-patt-b)" d="M0 0h200v200H0z" />
                 <path
                     d="M200 0H0v200"
                     fill="none"
@@ -53,7 +53,7 @@ function Canvas() {
             <svg viewBox={viewPort.box.join(" ")}>
                 <GridPattern />
                 <rect x={viewPort.box[0]} y={viewPort.box[1]} width="100%" height="100%" fill="#040d1c" /> #002846
-                <rect x={viewPort.box[0]} y={viewPort.box[1]} width="100%" height="100%" fill="url(#c)" />
+                <rect x={viewPort.box[0]} y={viewPort.box[1]} width="100%" height="100%" fill="url(#grid-patt-c)" />
                 <path d={svg.asString()} fill="white" stroke={"red"} strokeWidth={viewPort.strokeWidth} />
             </svg>
         </div>
