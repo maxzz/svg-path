7cf2040f36c327eae26c9fd3ff3e2c8bc7f5d539
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-25 17:47:11 -0800

    WIP. Canvas. useEventListener types. no

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
-type KeysMap = DocumentEventMap;
+// type KeysMap = DocumentEventMap;
 
-type EventHandler<T, K extends keyof DocumentEventMap> = (
-    this: T,
-    ev: DocumentEventMap[K],
-  ) => any
+// type EventHandler<T, K extends keyof DocumentEventMap> = (
+//     this: T,
+//     ev: DocumentEventMap[K],
+//   ) => any
 
 // export function useEventListener<T extends SVGElement, K extends keyof SVGElementEventMap>(
 //     type: K,
@@ -21,21 +21,25 @@ type EventHandler<T, K extends keyof DocumentEventMap> = (
 //     options?: boolean | AddEventListenerOptions
 // ): void;
 
-export function useEventListener<T extends Element | Document | Window, K extends keyof DocumentEventMap>(
-    type: K,
-    listener: EventHandler<T, K>,
-    ref: React.RefObject<T | null>,
-    options?: boolean | AddEventListenerOptions
-): void {
-    useEffect(() => {
-        const element = ref.current;
-        if (!element) {
-            return;
-        }
-        element.addEventListener(type, listener, options);
-        return () => element.removeEventListener(type, listener);
-    }, [type, listener, options, ref]);
-}
+
+
+// export function useEventListener<T extends Element | Document | Window, K extends keyof DocumentEventMap>(
+//     type: K,
+//     listener: EventHandler<T, K>,
+//     ref: React.RefObject<T | null>,
+//     options?: boolean | AddEventListenerOptions
+// ): void {
+//     useEffect(() => {
+//         const element = ref.current;
+//         if (!element) {
+//             return;
+//         }
+//         element.addEventListener(type, listener, options);
+//         return () => element.removeEventListener(type, listener);
+//     }, [type, listener, options, ref]);
+// }
+
+
 
 // export function useEventListener<T extends SVGElement, K extends keyof SVGElementEventMap>(
 //     type: K,
@@ -59,3 +63,70 @@ export function useEventListener<T extends Element | Document | Window, K extend
 //         return () => element.removeEventListener(type, listener);
 //     }, [type, listener, options, ref]);
 // }
+
+//https://github.com/react-restart/hooks/blob/master/src/useEventListener.ts
+
+/**
+ * Creates a `Ref` whose value is updated in an effect, ensuring the most recent
+ * value is the one rendered with. Generally only required for Concurrent mode usage
+ * where previous work in `render()` may be discarded before being used.
+ *
+ * This is safe to access in an event handler.
+ *
+ * @param value The `Ref` value
+ */
+ function useCommittedRef<TValue>(
+    value: TValue,
+  ): React.MutableRefObject<TValue> {
+    const ref = useRef(value)
+    useEffect(() => {
+      ref.current = value
+    }, [value])
+    return ref
+  }
+
+function useEventCallback<
+  TCallback extends (...args: any[]) => any
+>(fn?: TCallback | null): TCallback {
+  const ref = useCommittedRef(fn)
+  return useCallback(
+    function(...args: any[]) {
+      return ref.current && ref.current(...args)
+    },
+    [ref],
+  ) as any
+}
+
+type EventHandler<T, K extends keyof DocumentEventMap> = (
+    this: T,
+    ev: DocumentEventMap[K],
+  ) => any
+  
+  /**
+   * Attaches an event handler outside directly to specified DOM element
+   * bypassing the react synthetic event system.
+   *
+   * @param element The target to listen for events on
+   * @param event The DOM event name
+   * @param handler An event handler
+   * @param capture Whether or not to listen during the capture event phase
+   */
+  export default function useEventListener<
+    T extends Element | Document | Window,
+    K extends keyof DocumentEventMap
+  >(
+    eventTarget: T | (() => T),
+    event: K,
+    listener: EventHandler<T, K>,
+    capture: boolean | AddEventListenerOptions = false,
+  ) {
+    const handler = useEventCallback(listener)
+  
+    useEffect(() => {
+      const target =
+        typeof eventTarget === 'function' ? eventTarget() : eventTarget
+  
+      target.addEventListener(event, handler, capture)
+      return () => target.removeEventListener(event, handler, capture)
+    }, [eventTarget])
+  }
