6039a0ce49002608632d02baeed17e1ee161edae
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-28 16:44:00 -0800

    WIP. Canvas. add grid default background

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -63,11 +63,17 @@ function ControlPoint({ pt, stroke }: { pt: SvgControlPoint, stroke: number; }) 
 function BackgroundGrid({ x, y }: { x: number; y: number; }) {
     const [showGrid, setShowGrid] = useAtom(showGridAtom);
     return (
-        <g className="grid">
-            <GridPattern />
-            <rect x={x} y={y} width="100%" height="100%" fill="#040d1c" /> #002846
-            <rect x={x} y={y} width="100%" height="100%" fill="url(#grid-patt-c)" />
-        </g>
+        <>
+            {!showGrid ?
+                <g className="grid">
+                    <GridPattern />
+                    <rect x={x} y={y} width="100%" height="100%" fill="#040d1c" /> {/* #002846 */}
+                    <rect x={x} y={y} width="100%" height="100%" fill="url(#grid-patt-c)" />
+                </g>
+                :
+                <rect x={x} y={y} width="100%" height="100%" fill="#040d1c" />
+            }
+        </>
     );
 }
 
