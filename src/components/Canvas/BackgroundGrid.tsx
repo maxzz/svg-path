f215bc5ce2d015ab1d3adafb5a5175b8b30f741a
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-28 18:13:34 -0800

    WIP. Canvas. review ticks rendering. 2

--- src/components/Canvas/BackgroundGrid.tsx
+++ src/components/Canvas/BackgroundGrid.tsx
@@ -5,6 +5,7 @@ import { showGridAtom, showTicksAtom } from "../../store/store";
 function GridPattern({ x, y }: { x: number; y: number; }) {
     const [showGrid, setShowGrid] = useAtom(showGridAtom);
     const [showTicks, setShowTicks] = useAtom(showTicksAtom);
+    const fillName = showTicks ? 'url(#grid-patt-c)' : 'url(#grid-patt-a)';
     return (<>
         {showGrid && (<>
             <defs>
@@ -12,16 +13,20 @@ function GridPattern({ x, y }: { x: number; y: number; }) {
                 <pattern id="grid-patt-a" width={10} height={10} patternUnits="userSpaceOnUse">
                     <path d="M10 0H0v10" fill="none" stroke="#fff" strokeWidth={0.3} strokeOpacity={0.5} />
                 </pattern>
-                {/* 100-ticks */}
-                <pattern id="grid-patt-b" width={100} height={100} patternUnits="userSpaceOnUse">
-                    <path d="M0 0h100v100H0z" fill="url(#grid-patt-a)" />
-                    <path d="M100 0H0v100" fill="none" stroke="#fff" strokeWidth={2} strokeOpacity={0.2} />
-                </pattern>
-                {/* 200-ticks */}
-                <pattern id="grid-patt-c" width={200} height={200} patternUnits="userSpaceOnUse">
-                    <path d="M0 0h200v200H0z" fill="url(#grid-patt-b)" />
-                    <path d="M200 0H0v200" fill="none" stroke="#fff" strokeWidth={2} strokeOpacity={0.2} />
-                </pattern>
+                {showTicks &&
+                    <>
+                        {/* 100-ticks */}
+                        <pattern id="grid-patt-b" width={100} height={100} patternUnits="userSpaceOnUse">
+                            <path d="M0 0h100v100H0z" fill="url(#grid-patt-a)" />
+                            <path d="M100 0H0v100" fill="none" stroke="#fff" strokeWidth={2} strokeOpacity={0.2} />
+                        </pattern>
+                        {/* 200-ticks */}
+                        <pattern id="grid-patt-c" width={200} height={200} patternUnits="userSpaceOnUse">
+                            <path d="M0 0h200v200H0z" fill="url(#grid-patt-b)" />
+                            <path d="M200 0H0v200" fill="none" stroke="#fff" strokeWidth={2} strokeOpacity={0.2} />
+                        </pattern>
+                    </>
+                }
             </defs>
             <rect x={x} y={y} width="100%" height="100%" fill="url(#grid-patt-c)" />
         </>)}
