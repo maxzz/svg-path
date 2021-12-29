85526971326087c04e8cbeaa07cc488aa58276fc
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-28 18:28:23 -0800

    WIP. Canvas. fix the visual feedback of control buttons

--- src/components/Canvas/CanvasControlsPanel.tsx
+++ src/components/Canvas/CanvasControlsPanel.tsx
@@ -6,7 +6,7 @@ function Button({ label, atom }: { label: string; atom: WritableAtom<boolean, Se
     const [showGrid, setShowGrid] = useAtom(atom);
     return (
         <button
-            className={`px-1 py-0.5 text-sm text-slate-400 border-slate-500 border rounded ${showGrid ? 'bg-slate-600' : 'bg-slate-800'}`}
+            className={`px-1 py-0.5 text-sm text-slate-400 border-slate-500 border rounded ${showGrid ? 'bg-slate-800' : 'bg-slate-600'}`}
             onClick={() => setShowGrid((prev) => !prev)}
         >
             {label}
