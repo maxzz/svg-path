963808e61c4ce4940cbdd4e0e9db15037089a70d
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-28 19:12:56 -0800

    WIP. Points. add primitive event handlers

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import { mergeRef } from '../../hooks/utils';
 import { useAtom } from 'jotai';
-import { showGridAtom, svgAtom } from '../../store/store';
+import { activeCpPointAtom, activePathPointAtom, hoverCpPointAtom, hoverPathPointAtom, showGridAtom, svgAtom } from '../../store/store';
 import { useContainerZoom } from './useContainerZoom';
 import { SvgControlPoint, SvgPoint } from '../../svg/svg';
 import { BackgroundGrid } from './BackgroundGrid';
 import CanvasControlsPanel from './CanvasControlsPanel';
 import { ViewBox } from '../../svg/svg-utils';
 
-function TargetPoint({ pt, stroke }: { pt: SvgPoint, stroke: number; }) {
+function TargetPoint({ pt, stroke, idx }: { pt: SvgPoint, stroke: number; idx: number; }) {
+    const [activePathPt, setActivePathPt] = useAtom(activePathPointAtom);
+    const [activeCpPt, setActiveCpPt] = useAtom(activeCpPointAtom);
+    const [hoverPathPt, setHoverPathPt] = useAtom(hoverPathPointAtom);
+    const [hoverCpPt, setHoverCpPt] = useAtom(hoverCpPointAtom);
+
+    const active = activePathPt === idx;
+    const hover = hoverPathPt === idx;
     return (
-        <circle className="fill-[white]" cx={pt.x} cy={pt.y} r={stroke * 3} strokeWidth={stroke * 12} />
+        <circle
+            className={`${active ?'fill-[blue]':'fill-[white]'} ${hover ?'fill-[red]':'fill-[white]'}`}
+            cx={pt.x} cy={pt.y} r={stroke * 3} strokeWidth={stroke * 12}
+            onMouseEnter={(event) => {
+                console.log('mouse enter');
+                
+                setHoverPathPt(idx);
+                setHoverCpPt(-1);
+            }}
+            onMouseLeave={(event) => {
+                console.log('mouse leave');
+                setHoverPathPt(-1);
+            }}
+            onClick={() => {
+                console.log('click');
+                setActivePathPt(idx)
+            }}
+        />
     );
 }
 
-function ControlPoint({ pt, stroke }: { pt: SvgControlPoint, stroke: number; }) {
+function ControlPoint({ pt, stroke, idx }: { pt: SvgControlPoint, stroke: number; idx: number; }) {
     return (
         <>
             <circle className="fill-[white]" cx={pt.x} cy={pt.y} r={stroke * 3} strokeWidth={stroke * 12} />
@@ -27,28 +51,28 @@ function ControlPoint({ pt, stroke }: { pt: SvgControlPoint, stroke: number; }) 
     );
 }
 
-function SvgCanvas({viewBox, viewBoxStroke}: {viewBox: ViewBox; viewBoxStroke: number}) {
+function SvgCanvas({ viewBox, viewBoxStroke }: { viewBox: ViewBox; viewBoxStroke: number; }) {
     const [svg] = useAtom(svgAtom);
     const pathPoints = svg.targetLocations();
     const cpPoints = svg.controlLocations();
     return (
         <svg viewBox={viewBox.join(" ")}>
-        <BackgroundGrid x={viewBox[0]} y={viewBox[1]} />
+            <BackgroundGrid x={viewBox[0]} y={viewBox[1]} />
 
-        <path d={svg.asString()} fill="#94a3b830" stroke="white" strokeWidth={viewBoxStroke} />
+            <path d={svg.asString()} fill="#94a3b830" stroke="white" strokeWidth={viewBoxStroke} />
 
-        <g className="pathPts">
-            {pathPoints.map((pt, idx) => (
-                <TargetPoint pt={pt} stroke={viewBoxStroke} key={idx} />
-            ))}
-        </g>
+            <g className="pathPts">
+                {pathPoints.map((pt, idx) => (
+                    <TargetPoint pt={pt} stroke={viewBoxStroke} idx={idx} key={idx} />
+                ))}
+            </g>
 
-        <g className="cpPts">
-            {cpPoints.map((pt, idx) => (
-                <ControlPoint pt={pt} stroke={viewBoxStroke} key={idx} />
-            ))}
-        </g>
-    </svg>
+            <g className="cpPts">
+                {cpPoints.map((pt, idx) => (
+                    <ControlPoint pt={pt} stroke={viewBoxStroke} idx={idx} key={idx} />
+                ))}
+            </g>
+        </svg>
     );
 }
 
