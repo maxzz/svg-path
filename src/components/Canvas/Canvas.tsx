bf5d07080f322e78efdc84956b44ec804bbcec32
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-29 23:58:27 -0800

    WIP. Control points. add back tracking index detection

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -58,6 +58,8 @@ function ControlPoint({ svgItem, pathPoints, pt, stroke, idx }: { svgItem: SvgIt
     const [activePt, setActivePt] = useAtom(activePointAtom);
     const [hoverPt, setHoverPt] = useAtom(hoverPointAtom);
 
+    const thisIdx = pathPoints.findIndex((pt) => pt === svgItem.itemReference);
+
     const active = activePt === idx;
     const hover = hoverPt === idx;
 
