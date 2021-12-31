86548b1864f97449f20b77c63496bc5547d7582e
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-30 23:47:04 -0800

    WIP. Points editor. code clean up

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -4,7 +4,7 @@ import { activePointAtom, svgAtom } from "../../store/store";
 import { SvgItem } from "../../svg/svg";
 import { IconMenu } from "../UI/icons/Icons";
 
-function CommandName({ command, abs }: { command: string; abs: boolean; }) {
+function PointName({ command, abs }: { command: string; abs: boolean; }) {
     return (
         <label className={`flex-0 px-1 w-6 leading-3 text-xs rounded-l-[0.2rem] text-center text-slate-900 bg-slate-500 focus-within:text-blue-500 overflow-hidden`}>
             {/* <input className="px-1 w-full text-xs text-center text-slate-900 bg-slate-500 focus:outline-none" defaultValue={"M"} /> */}
@@ -13,7 +13,7 @@ function CommandName({ command, abs }: { command: string; abs: boolean; }) {
     );
 }
 
-function CommandInput({ atom }: { atom: PrimitiveAtom<number>; }) {
+function PointValue({ atom }: { atom: PrimitiveAtom<number>; }) {
     const [value, setValue] = useAtom(atom);
     return (
         <label className={`flex-1 max-w-[2rem] rounded-tl-sm overflow-hidden bg-slate-200 focus-within:text-blue-500`}>
@@ -26,7 +26,7 @@ function CommandInput({ atom }: { atom: PrimitiveAtom<number>; }) {
     );
 }
 
-function OneCommand({ path, idx }: { path: SvgItem; idx: number; }) {
+function CommandRow({ path, idx }: { path: SvgItem; idx: number; }) {
     const createAtoms = () => path.values.map((value) => atom(value));
 
     const [valuesAtoms, setValuesAtoms] = React.useState(createAtoms());
@@ -39,14 +39,16 @@ function OneCommand({ path, idx }: { path: SvgItem; idx: number; }) {
 
     return (<>
         <div className={`flex items-center justify-between ${active ? 'bg-blue-300' : ''}`}>
-
+            
+            {/* Values */}
             <div className="flex items-center justify-items-start font-mono space-x-0.5">
-                <CommandName command={path.getType()} abs={false} />
+                <PointName command={path.getType()} abs={false} />
                 {valuesAtoms.map((atom, idx) => (
-                    <CommandInput atom={atom} key={idx} />
+                    <PointValue atom={atom} key={idx} />
                 ))}
             </div>
 
+            {/* Menu */}
             <button className="flex-0 px-1 mt-0.5 active:scale-[.97]">
                 <IconMenu className="w-4 h-4" />
             </button>
@@ -59,7 +61,7 @@ export function PathCommandEditor() {
     return (
         <div className="my-1 space-y-0.5">
             {svg.path.map((path, idx) => (
-                <OneCommand path={path} idx={idx} key={idx} />
+                <CommandRow path={path} idx={idx} key={idx} />
             ))}
         </div >
     );
