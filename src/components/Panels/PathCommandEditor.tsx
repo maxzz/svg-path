3dff8f13b2904c6f37cba1a7abdd5a836cc169f3
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-30 23:58:09 -0800

    WIP. Points editor. fix command activation on focus

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -5,12 +5,12 @@ import { activePointAtom, svgAtom } from "../../store/store";
 import { SvgItem } from "../../svg/svg";
 import { IconMenu } from "../UI/icons/Icons";
 
-function PointName({ idx, command, abs }: { idx: number; command: string; abs: boolean; }) {
+function PointName({ pathIdx, command, abs }: { pathIdx: number; command: string; abs: boolean; }) {
     const setActivePoint = useUpdateAtom(activePointAtom);
     return (
         <label
             className={`flex-0 px-1 w-6 leading-3 text-xs rounded-l-[0.2rem] text-center text-slate-900 bg-slate-500 focus-within:text-blue-500 overflow-hidden`}
-            onBlur={() => setActivePoint(idx)}
+            onFocus={() => setActivePoint(pathIdx)}
         >
             {/* <input className="px-1 w-full text-xs text-center text-slate-900 bg-slate-500 focus:outline-none" defaultValue={"M"} /> */}
             <button className="py-1">{command}</button>
@@ -18,7 +18,7 @@ function PointName({ idx, command, abs }: { idx: number; command: string; abs: b
     );
 }
 
-function PointValue({ idx, atom }: { idx: number; atom: PrimitiveAtom<number>; }) {
+function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<number>; }) {
     const [value, setValue] = useAtom(atom);
     return (
         <label className={`flex-1 max-w-[2rem] rounded-tl-sm overflow-hidden bg-slate-200 focus-within:text-blue-500`}>
@@ -31,7 +31,7 @@ function PointValue({ idx, atom }: { idx: number; atom: PrimitiveAtom<number>; }
     );
 }
 
-function CommandRow({ path, idx }: { path: SvgItem; idx: number; }) {
+function CommandRow({ path, pathIdx }: { path: SvgItem; pathIdx: number; }) {
     const createAtoms = () => path.values.map((value) => atom(value));
 
     const [valuesAtoms, setValuesAtoms] = React.useState(createAtoms());
@@ -40,16 +40,17 @@ function CommandRow({ path, idx }: { path: SvgItem; idx: number; }) {
     }, [path]);
 
     const [activePoint] = useAtom(activePointAtom);
-    const active = activePoint === idx;
+    const active = activePoint === pathIdx;
 
     return (<>
         <div className={`flex items-center justify-between ${active ? 'bg-blue-300' : ''}`}>
 
             {/* Values */}
             <div className="flex items-center justify-items-start font-mono space-x-0.5">
-                <PointName idx={idx} command={path.getType()} abs={false} />
+                <PointName pathIdx={pathIdx} command={path.getType()} abs={false} />
+
                 {valuesAtoms.map((atom, idx) => (
-                    <PointValue idx={idx} atom={atom} key={idx} />
+                    <PointValue pathIdx={pathIdx} atom={atom} key={idx} />
                 ))}
             </div>
 
@@ -66,7 +67,7 @@ export function PathCommandEditor() {
     return (
         <div className="my-1 space-y-0.5">
             {svg.path.map((path, idx) => (
-                <CommandRow path={path} idx={idx} key={idx} />
+                <CommandRow path={path} pathIdx={idx} key={idx} />
             ))}
         </div >
     );
