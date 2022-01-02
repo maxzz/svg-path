eb92f447f704d99a4788bb0285e2772cddab319c
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-01 19:41:07 -0800

    WIP. Points editor. code clean up

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -21,10 +21,8 @@ function PointName({ pathIdx, command, abs }: { pathIdx: number; command: string
 function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<number>; }) {
     const [value, setValue] = useAtom(atom);
 
-    const [local, setLocal] = React.useState(''+value);
-    React.useEffect(() => {
-        setLocal(''+value);
-    }, [value]);
+    const [local, setLocal] = React.useState('' + value);
+    React.useEffect(() => setLocal('' + value), [value]);
 
     function convertToNumber(s: string) {
         s = s.replace(/[\u066B,]/g, '.').replace(/[^\-0-9.eE]/g, ''); //disable type illegal chars. \u066B unicode-arabic-decimal-separator
@@ -32,19 +30,13 @@ function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<nu
         setLocal(s);
         s && !isNaN(v) && setValue(v);
     }
+
     function resetInvalid() {
-        (!local || isNaN(+local)) && setLocal(''+value);
+        (!local || isNaN(+local)) && setLocal('' + value);
     }
 
-    console.log('value', value);
-    
-
-    //const setActivePoint = useUpdateAtom(activePointAtom);
     return (
-        <label
-            className={`flex-1 max-w-[3rem] rounded-tl-sm overflow-hidden bg-slate-200 focus-within:text-blue-500`}
-        // onFocus={() => setActivePoint(pathIdx)}
-        >
+        <label className={`flex-1 max-w-[3rem] rounded-tl-sm overflow-hidden bg-slate-200 focus-within:text-blue-500`}>
             <input
                 className="px-px pb-[4px] w-full h-full text-[10px] text-center tracking-tighter text-slate-900 focus:border-blue-500 bg-slate-200 focus:outline-none"
                 value={local}
@@ -54,29 +46,6 @@ function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<nu
         </label>
     );
 }
-
-// function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<number>; }) {
-//     const [value, setValue] = useAtom(atom);
-//     const setActivePoint = useUpdateAtom(activePointAtom);
-//     function convertToNumber(s: string) {
-//         const v = +s;
-//         if (!isNaN(v)) {
-//             setValue(+s);
-//         }
-//     }
-//     return (
-//         <label
-//             className={`flex-1 max-w-[3rem] rounded-tl-sm overflow-hidden bg-slate-200 focus-within:text-blue-500`}
-//             // onFocus={() => setActivePoint(pathIdx)}
-//         >
-//             <input
-//                 className="px-px pb-[4px] w-full h-full text-[10px] text-center tracking-tighter text-slate-900 focus:border-blue-500 bg-slate-200 focus:outline-none"
-//                 value={value}
-//                 onChange={(event) => convertToNumber(event.target.value)}
-//             />
-//         </label>
-//     );
-// }
 
 //TODO: switch to grid (first row problem)
 
@@ -95,7 +64,7 @@ function CommandRow({ path, pathIdx }: { path: SvgItem; pathIdx: number; }) {
         <div
             className={`flex items-center justify-between ${active ? 'bg-blue-300' : ''}`}
             onClick={() => setActivePoint(pathIdx)}
-            // onFocus={() => console.log('child')}
+        // onFocus={() => console.log('child')}
         >
             {/* Values */}
             <div className="flex items-center justify-items-start font-mono space-x-0.5">
