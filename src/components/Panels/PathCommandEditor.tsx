f50bcf546356d6e6a2be6daad1b39c8decd7b304
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-01 19:43:36 -0800

    WIP. Points editor. code clean up

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -1,16 +1,13 @@
 import { atom, PrimitiveAtom, useAtom } from "jotai";
-import { useUpdateAtom } from "jotai/utils";
 import React, { useEffect } from "react";
 import { activePointAtom, svgAtom } from "../../store/store";
 import { SvgItem } from "../../svg/svg";
 import { IconMenu } from "../UI/icons/Icons";
 
 function PointName({ pathIdx, command, abs }: { pathIdx: number; command: string; abs: boolean; }) {
-    const setActivePoint = useUpdateAtom(activePointAtom);
     return (
         <label
             className={`flex-0 px-1 w-6 leading-3 text-xs rounded-l-[0.2rem] text-center text-slate-900 bg-slate-400 focus-within:text-blue-500 overflow-hidden`}
-        // onFocus={() => setActivePoint(pathIdx)}
         >
             {/* <input className="px-1 w-full text-xs text-center text-slate-900 bg-slate-500 focus:outline-none" defaultValue={"M"} /> */}
             <button className="py-1">{command}</button>
@@ -20,7 +17,6 @@ function PointName({ pathIdx, command, abs }: { pathIdx: number; command: string
 
 function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<number>; }) {
     const [value, setValue] = useAtom(atom);
-
     const [local, setLocal] = React.useState('' + value);
     React.useEffect(() => setLocal('' + value), [value]);
 
@@ -64,7 +60,6 @@ function CommandRow({ path, pathIdx }: { path: SvgItem; pathIdx: number; }) {
         <div
             className={`flex items-center justify-between ${active ? 'bg-blue-300' : ''}`}
             onClick={() => setActivePoint(pathIdx)}
-        // onFocus={() => console.log('child')}
         >
             {/* Values */}
             <div className="flex items-center justify-items-start font-mono space-x-0.5">
