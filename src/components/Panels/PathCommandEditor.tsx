0cbbb0fd212b10b2e5d3e64ff8a5a5d88bb79888
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-02 01:43:27 -0800

    WIP. Points editor. fix monitor is using stale values.

--- src/components/Panels/PathCommandEditor.tsx
+++ src/components/Panels/PathCommandEditor.tsx
@@ -45,6 +45,7 @@ function PointValue({ pathIdx, atom }: { pathIdx: number; atom: PrimitiveAtom<nu
 }
 
 //TODO: switch to grid (first row problem)
+//TODO: check scale when only one 'm' command
 
 const createRowAtoms = (values: number[], monitor: OnValueChange<number>) => {
     return values.map((value) => atomWithCallback(value, monitor));
@@ -52,54 +53,64 @@ const createRowAtoms = (values: number[], monitor: OnValueChange<number>) => {
 
 function CommandRow({ svgItem, svgItemIdx }: { svgItem: SvgItem; svgItemIdx: number; }) {
 
-    const [masterAtom, setMasterAtom] = React.useState(atom([]));
-    const [master, setmaster] = useAtom(masterAtom);
-
-    console.log('render', svgItem.asString());
-    
-
-    useEffect(() => {
-        console.log('effect', svgItem.asString());
-    }, [svgItem]);
-
-/*
-    // const onAtomChange = React.useCallback<OnValueChange<number>>(({ get, set }) => {
-    //     console.log('valueAtoms', valueAtoms.map(a => a.toString()));
-
-    //     const res = valueAtoms.map((valueAtom) => get(valueAtom)).join(',');
-    //     console.log('changed', res);
-    // }, []);
+    const [masterAtom, setMasterAtom] = React.useState(atom<WritableAtom<number, SetStateAction<number>>[]>([]));
+    const [master, setMaster] = useAtom(masterAtom);
 
     const onAtomChange = React.useCallback<OnValueChange<number>>(({ get, set }) => {
-        const aa = getAtoms();
-        console.log('valueAtoms', aa.map(a => a.toString()));
-
-        const res = aa.map((valueAtom) => get(valueAtom)).join(',');
-        console.log('changed', res);
+        const aa = get(masterAtom);
+        console.log('onAtomChange aa', aa.map(a => a.toString()), 'val', aa.map(a => get(a)));
     }, []);
 
-    function getAtoms() {
-        return valueAtoms;
-    }
-
-    // const onAtomChange: OnValueChange<number> = ({ get, set }) => {
-    //     console.log('valueAtoms', valueAtoms.map(a => a.toString()));
-
-    //     const res = valueAtoms.map((valueAtom) => get(valueAtom)).join(',');
-    //     console.log('changed', res);
-    // };
-
-    // const [valueAtoms, setValuesAtoms] = React.useState(createRowAtoms(svgItem.values, onAtomChange));
-    const [valueAtoms, setValuesAtoms] = React.useState<WritableAtom<number, SetStateAction<number>>[]>([]);
     useEffect(() => {
-        const a = createRowAtoms(svgItem.values, onAtomChange);
-        console.log('update atoms', a.map(a => a.toString()));
-
-        setValuesAtoms(a);
+        const atoms = createRowAtoms(svgItem.values, onAtomChange);
+        setMaster(atoms);
     }, [svgItem]);
 
-    console.log('main valueAtoms', valueAtoms.map(a => a.toString()), svgItem.asString());
-*/
+    /*
+        console.log('render', svgItem.asString());
+        useEffect(() => {
+            console.log('effect', svgItem.asString());
+        }, [svgItem]);
+    */
+
+    /*
+        // const onAtomChange = React.useCallback<OnValueChange<number>>(({ get, set }) => {
+        //     console.log('valueAtoms', valueAtoms.map(a => a.toString()));
+    
+        //     const res = valueAtoms.map((valueAtom) => get(valueAtom)).join(',');
+        //     console.log('changed', res);
+        // }, []);
+    
+        const onAtomChange = React.useCallback<OnValueChange<number>>(({ get, set }) => {
+            const aa = getAtoms();
+            console.log('valueAtoms', aa.map(a => a.toString()));
+    
+            const res = aa.map((valueAtom) => get(valueAtom)).join(',');
+            console.log('changed', res);
+        }, []);
+    
+        function getAtoms() {
+            return valueAtoms;
+        }
+    
+        // const onAtomChange: OnValueChange<number> = ({ get, set }) => {
+        //     console.log('valueAtoms', valueAtoms.map(a => a.toString()));
+    
+        //     const res = valueAtoms.map((valueAtom) => get(valueAtom)).join(',');
+        //     console.log('changed', res);
+        // };
+    
+        // const [valueAtoms, setValuesAtoms] = React.useState(createRowAtoms(svgItem.values, onAtomChange));
+        const [valueAtoms, setValuesAtoms] = React.useState<WritableAtom<number, SetStateAction<number>>[]>([]);
+        useEffect(() => {
+            const a = createRowAtoms(svgItem.values, onAtomChange);
+            console.log('update atoms', a.map(a => a.toString()));
+    
+            setValuesAtoms(a);
+        }, [svgItem]);
+    
+        console.log('main valueAtoms', valueAtoms.map(a => a.toString()), svgItem.asString());
+    */
     const [activePoint, setActivePoint] = useAtom(activePointAtom);
     const active = activePoint === svgItemIdx;
 
@@ -112,6 +123,9 @@ function CommandRow({ svgItem, svgItemIdx }: { svgItem: SvgItem; svgItemIdx: num
             <div className="flex items-center justify-items-start font-mono space-x-0.5">
                 <PointName pathIdx={svgItemIdx} command={svgItem.getType()} abs={false} />
 
+                {master.map((atom, idx) => (
+                    <PointValue pathIdx={svgItemIdx} atom={atom} key={idx} />
+                ))}
                 {/* {valueAtoms.map((atom, idx) => (
                     <PointValue pathIdx={svgItemIdx} atom={atom} key={idx} />
                 ))} */}
@@ -126,6 +140,7 @@ function CommandRow({ svgItem, svgItemIdx }: { svgItem: SvgItem; svgItemIdx: num
 }
 
 //M61.27.5c45.13.16 68.384 25.592 96.354 57.072 27.98 31.47 41.564 54.925 75.536 54.478 33.972-.447 50.78-22.13 50.78-50.78 0-28.65-14.587-51.595-50.78-50.77-36.193.825-60.4 17.052-88.2 47.072-27.8 30.01-40.15 64.308-83.68 64.478C17.74 122.21.5 89.93.5 61.28.5 32.63 16.14.34 61.27.5z
+//M61.27.5 C17.74 122.21.5 89.93.5 61.28.5 32.63 16.14.34 61.27.5
 
 export function PathCommandEditor() {
     const [svg] = useAtom(svgAtom);
