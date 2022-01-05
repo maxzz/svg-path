fc40f75e5c3df7c11f5df3252795de85d3a4a245
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-04 16:56:17 -0800

    Add PanelPath as a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,12 @@
 import React, { useState } from 'react';
 import { atom, useAtom } from 'jotai';
 import { Accordion } from './components/UI/Accordion';
-import { pathUnsafeAtom } from './store/store';
 import { PathCommandEditor } from './components/Panels/PathCommandEditor';
 import { PathCanvas } from './components/Canvas/Canvas';
 import './App.css';
 import { SectionPane } from './components/UI/SectionPane';
 import { PanelOperations } from './components/Panels/PanelOperations';
-
-function PathEditor() {
-    const [path, setPath] = useAtom(pathUnsafeAtom);
-    return (
-        <textarea
-            className="w-full bg-slate-200 font-mono text-xs"
-            rows={5}
-            spellCheck={false}
-            value={path}
-            onChange={(event) => setPath(event.target.value)}
-        />
-    );
-}
-
-function PanelPath() {
-    const [openAtom] = useState(atom(true));
-    const [open, setOpen] = useAtom(openAtom);
-    return (
-        <div className="">
-            <SectionPane open={open} onClick={() => setOpen(v => !v)}>
-                Path
-            </SectionPane>
-            <Accordion toggle={open}>
-                <div className="px-1 text-sm bg-slate-300 overflow-hidden">
-                    <div className="flex justify-between">
-                        <div className="text-xs tracking-tighter self-end">path</div>
-                        <div className="py-1 flex space-x-1">
-                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Open</button>
-                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Save</button>
-                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Clear</button>
-                        </div>
-                    </div>
-
-                    <label>
-                        <PathEditor />
-                    </label>
-                </div>
-            </Accordion>
-        </div>
-    );
-}
+import { PanelPath } from './components/Panels/PanelPath';
 
 function PanelOptions() {
     const [openAtom] = useState(atom(true));
---
+++ src/components/Panels/PanelPath.tsx
@@ -0,0 +1,46 @@
+import { atom, useAtom } from 'jotai';
+import React, { useState } from 'react';
+import { pathUnsafeAtom } from '../../store/store';
+import { Accordion } from '../UI/Accordion';
+import { SectionPane } from '../UI/SectionPane';
+
+function PathEditor() {
+    const [path, setPath] = useAtom(pathUnsafeAtom);
+    return (
+        <textarea
+            className="w-full bg-slate-200 font-mono text-xs"
+            rows={5}
+            spellCheck={false}
+            value={path}
+            onChange={(event) => setPath(event.target.value)}
+        />
+    );
+}
+
+export function PanelPath() {
+    const [openAtom] = useState(atom(true));
+    const [open, setOpen] = useAtom(openAtom);
+    return (
+        <div className="">
+            <SectionPane open={open} onClick={() => setOpen(v => !v)}>
+                Path
+            </SectionPane>
+            <Accordion toggle={open}>
+                <div className="px-1 text-sm bg-slate-300 overflow-hidden">
+                    <div className="flex justify-between">
+                        <div className="text-xs tracking-tighter self-end">path</div>
+                        <div className="py-1 flex space-x-1">
+                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Open</button>
+                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Save</button>
+                            <button className="px-1 pt-0.5 pb-1 bg-slate-400/40 border-slate-400 border rounded shadow-sm active:scale-[.97]">Clear</button>
+                        </div>
+                    </div>
+
+                    <label>
+                        <PathEditor />
+                    </label>
+                </div>
+            </Accordion>
+        </div>
+    );
+}
