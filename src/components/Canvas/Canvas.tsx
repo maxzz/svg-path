dd987a6bbf727df9806c62a4850c54b1d3a5bbe0
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-08 15:47:41 -0800

    WIP. new canvas. move out canvas points business out of canvas

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,68 +1,14 @@
 import React from 'react';
 import { mergeRef } from '../../hooks/utils';
 import { useAtom } from 'jotai';
-import { activePointAtom, hoverPointAtom, showGridAtom, svgAtom } from '../../store/store';
+import { activePointAtom, svgAtom } from '../../store/store';
 import { useContainerZoom } from './useContainerZoom';
-import { formatNumber, SvgControlPoint, SvgItem, SvgPoint } from '../../svg/svg';
+import { SvgItem, SvgPoint } from '../../svg/svg';
 import { BackgroundGrid } from './BackgroundGrid';
 import CanvasControlsPanel from './CanvasControlsPanel';
 import { ViewBox } from '../../svg/svg-utils-viewport';
 import { useUpdateAtom } from 'jotai/utils';
-
-const ptColor = (active: boolean, hover: boolean): string => active ? '#009cff' : hover ? '#ff4343' : 'white';
-
-function TargetPoint({ svgItem, pt, stroke, idx }: { svgItem: SvgItem; pt: SvgPoint, stroke: number; idx: number; }) {
-    const [activePt, setActivePt] = useAtom(activePointAtom);
-    const [hoverPt, setHoverPt] = useAtom(hoverPointAtom);
-    const active = activePt === idx;
-    const hover = hoverPt === idx;
-    return (<>
-        {(active || hover) &&
-            <path style={{ stroke: ptColor(active, hover), fill: 'none' }} strokeWidth={stroke} d={svgItem.asStandaloneString()} />
-        }
-        <circle
-            className="cursor-pointer"
-            style={{ stroke: 'transparent', fill: ptColor(active, hover) }}
-            cx={pt.x} cy={pt.y} r={stroke * 3} strokeWidth={stroke * 12}
-
-            onMouseEnter={(event) => { event.stopPropagation(); setHoverPt(idx); }}
-            onMouseLeave={() => setHoverPt(-1)}
-            onMouseDown={() => {
-                setActivePt(idx);
-            }}
-        >
-            <title>abs: {formatNumber(pt.x, 2)},{formatNumber(pt.y, 2)}</title>
-        </circle>
-    </>);
-}
-
-function ControlPoint({ pt, stroke, idx }: { pt: SvgControlPoint, stroke: number; idx: number; }) {
-    const [activePt, setActivePt] = useAtom(activePointAtom);
-    const [hoverPt, setHoverPt] = useAtom(hoverPointAtom);
-    const active = activePt === idx;
-    const hover = hoverPt === idx;
-    return (<>
-        {pt.relations.map((rel, idx) => (
-            <line
-                className="stroke-[#fff7]"
-                x1={pt.x} y1={pt.y} x2={rel.x} y2={rel.y} strokeWidth={stroke} key={idx}
-            />
-        ))}
-        <rect
-            className="cursor-pointer"
-            style={{ stroke: 'transparent', fill: ptColor(active, hover) }}
-            x={pt.x - 3 * stroke} y={pt.y - 3 * stroke} width={stroke * 6} height={stroke * 6} strokeWidth={stroke * 12}
-
-            onMouseEnter={(event) => { event.stopPropagation(); setHoverPt(idx); }}
-            onMouseLeave={() => setHoverPt(-1)}
-            onMouseDown={() => {
-                setActivePt(idx);
-            }}
-        >
-            <title>abs: {formatNumber(pt.x, 2)},{formatNumber(pt.y, 2)}</title>
-        </rect>
-    </>);
-}
+import { ControlPoint, TargetPoint } from './CanvasPoints';
 
 const cpToTargetIdx = (targetLocations: SvgPoint[], ref: SvgItem) => targetLocations.findIndex((pt) => pt.itemReference === ref);
 
