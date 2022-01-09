d0aa40727aa3c8bb8d73557ae04b6c636e85a0d5
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-09 00:54:24 -0800

    WIP. new canvas. connect ticks input control

--- src/components/Canvas/CanvasControlsPanel.tsx
+++ src/components/Canvas/CanvasControlsPanel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SetStateAction, useAtom, WritableAtom } from 'jotai';
-import { showGridAtom, showTicksAtom } from '../../store/store';
+import { showGridAtom, showTicksAtom, tickIntevalAtom } from '../../store/store';
 //import { AccordionHorizontal } from '../UI/Accordion';
 
 function Button({ label, atom }: { label: string; atom: WritableAtom<boolean, SetStateAction<boolean>, void>; }) {
@@ -43,12 +43,17 @@ export function CanvasControlsPanel() {
 
 export function AppCommands() {
     const [showGrid] = useAtom(showGridAtom);
+    const [tickInteval, setTickInteval] = useAtom(tickIntevalAtom);
     return (
         <div className="absolute bottom-4 right-4 px-2 py-2 bg-slate-400/40 rounded flex items-center space-x-2">
 
             {showGrid &&
                 <div className="flex items-center ">
-                    <input className="px-1 w-5 h-6 text-xs rounded-l border border-slate-500  text-slate-400 bg-slate-700 shadow-sm shadow-slate-800" />
+                    <input
+                        className="w-6 h-6 text-sm text-center rounded-l border border-slate-500  text-slate-400 bg-slate-700 shadow-sm shadow-slate-800"
+                        value={tickInteval}
+                        onChange={(event) => setTickInteval(+event.target.value)}
+                    />
 
                     <ButtonWoLeft label="Ticks" atom={showTicksAtom} />
                 </div>
