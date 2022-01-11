c19620daff7dc49a428e0765e961ece6749d9217
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-10 16:53:39 -0800

    WIP. new canvas. events. add class DragPoint as bridge between classes and hooks

--- src/components/Canvas/CanvasPoints.tsx
+++ src/components/Canvas/CanvasPoints.tsx
@@ -1,14 +1,46 @@
-import { useAtom } from "jotai";
+import { atom, useAtom } from "jotai";
+import { useUpdateAtom } from "jotai/utils";
 import { activePointAtom, hoverPointAtom } from "../../store/store";
 import { formatNumber, SvgControlPoint, SvgItem, SvgPoint } from "../../svg/svg";
+import { ViewPoint } from "../../svg/svg-utils-viewport";
 
 const ptColor = (active: boolean, hover: boolean): string => active ? '#009cff' : hover ? '#ff4343' : 'white';
+
+class DragPoint {
+    constructor(public dragItem: SvgItem, startPt: ViewPoint) {
+        console.log('onMouseDown', this.dragItem);
+    }
+    onDrag(event: MouseEvent) {
+        console.log('onMouseDown', this.dragItem, event.target);
+    }
+    onDragEnd(event: MouseEvent) {
+        console.log('onMouseDown', this.dragItem, event.target);
+    }
+}
+
+const _DragPointAtom = atom<DragPoint | null>(null);
+const DragPointEventAtom = atom(null, (get, set, { event, start, end }: { event: MouseEvent, start?: SvgItem, end?: boolean; }) => {
+    if (start) {
+        set(_DragPointAtom, new DragPoint(start, { x: event.clientX, y: event.clientY }));
+    } else {
+        const dp = get(_DragPointAtom);
+        if (!dp) { return; }
+        if (end) {
+            dp.onDragEnd(event);
+            set(_DragPointAtom, null);
+        } else {
+            dp.onDrag(event);
+        }
+    }
+});
 
 export function TargetPoint({ svgItem, pt, stroke, idx }: { svgItem: SvgItem; pt: SvgPoint, stroke: number; idx: number; }) {
     const [activePt, setActivePt] = useAtom(activePointAtom);
     const [hoverPt, setHoverPt] = useAtom(hoverPointAtom);
     const active = activePt === idx;
     const hover = hoverPt === idx;
+
+    const setDragPointEvent = useUpdateAtom(DragPointEventAtom);
     return (<>
         {(active || hover) &&
             <path style={{ stroke: ptColor(active, hover), fill: 'none' }} strokeWidth={stroke} d={svgItem.asStandaloneString()} />
@@ -23,9 +55,14 @@ export function TargetPoint({ svgItem, pt, stroke, idx }: { svgItem: SvgItem; pt
             onMouseDown={(event) => {
                 event.stopPropagation();
                 setActivePt(idx);
+                setDragPointEvent({event: event.nativeEvent, start: svgItem});
+            }}
+            onMouseMove={(event) => {
+                setDragPointEvent({event: event.nativeEvent});
             }}
             onMouseUp={(event) => {
                 event.stopPropagation();
+                setDragPointEvent({event: event.nativeEvent, end: true});
             }}
         >
             <title>abs: {formatNumber(pt.x, 2)},{formatNumber(pt.y, 2)}</title>
