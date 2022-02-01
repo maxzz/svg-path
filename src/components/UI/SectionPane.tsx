c1aa0c3a1c56dd2b2dc394a0f67f4a800fa00736
Max Zakharzhevskiy <maxzz@msn.com>
2022-02-01 00:37:29 -0800

    WIP. Re-implement section arrow animation w/ react-spring vs. framer-motion; saved gzip 100K

--- src/components/UI/SectionPane.tsx
+++ src/components/UI/SectionPane.tsx
@@ -1,31 +1,56 @@
-import { motion, useAnimation, Variants } from 'framer-motion';
 import React, { HTMLAttributes, ReactNode } from 'react';
+import { a, useSpring } from '@react-spring/web';
 
-const variants: Variants = {
-    open: { d: ["M 20,65 50,35 80,65", "M 20,35 50,65 80,35"], },
-    closed: { d: ["M 20,35 50,65 80,35", "M 20,65 50,35 80,65"], },
-};
-
-export function SectionPane({ children, open = true, onClick, ...rest }: { children?: ReactNode; open?: boolean; } & HTMLAttributes<HTMLDivElement>) {
-    const api = useAnimation();
-    const click = (event: React.MouseEvent<HTMLDivElement>) => onClick && (api.start(open ? "closed" : "open"), onClick(event));
+export function SectionPane({ children, open = true, ...rest }: { open?: boolean; } & HTMLAttributes<HTMLDivElement>) {
+    const styles = useSpring({ open: open ? 1 : 0 });
     return (
         <div
             className="px-2 py-1 bg-slate-500 text-stone-100 uppercase flex items-center justify-between select-none cursor-pointer font-ui"
-            onClick={click}
             {...rest}
         >
-            <div className="pr-1 pt-1">{children}</div>
-            <div className="">
-                <svg className="w-6 h-6 p-1 stroke-current stroke-[.6rem] fill-none" viewBox="0 0 100 100">
-                    <motion.path
-                        d="M 20,35 50,65 80,35"
-                        animate={api}
-                        variants={variants}
-                        transition={{ ease: "easeInOut", duration: .2, }}
-                    />
-                </svg>
+            {/* Section name */}
+            <div className="pr-1 pt-1">
+                {children}
             </div>
+            {/* Open/Close icon */}
+            <svg className="w-6 h-6 p-1 stroke-current stroke-[.6rem] fill-none" viewBox="0 0 100 100">
+                <a.path d={styles.open.to({ range: [0, 1], output: ["M 20,65 50,35 80,65", "M 20,35 50,65 80,35"] })} />
+            </svg>
         </div>
     );
 }
+
+
+// The code below adds gzip 100K, but useSpring above does the same. (unzipped 849KB -> 545KB)
+
+
+// import { motion, useAnimation, Variants } from 'framer-motion';
+
+// const variants: Variants = {
+//     open: { d: ["M 20,65 50,35 80,65", "M 20,35 50,65 80,35"], },
+//     closed: { d: ["M 20,35 50,65 80,35", "M 20,65 50,35 80,65"], },
+// };
+
+// export function SectionPane({ children, open = true, onClick, ...rest }: { children?: ReactNode; open?: boolean; } & HTMLAttributes<HTMLDivElement>) {
+//     const api = useAnimation();
+//     const click = (event: React.MouseEvent<HTMLDivElement>) => onClick && (api.start(open ? "closed" : "open"), onClick(event));
+//     return (
+//         <div
+//             className="px-2 py-1 bg-slate-500 text-stone-100 uppercase flex items-center justify-between select-none cursor-pointer font-ui"
+//             onClick={click}
+//             {...rest}
+//         >
+//             <div className="pr-1 pt-1">{children}</div>
+//             <div className="">
+//                 <svg className="w-6 h-6 p-1 stroke-current stroke-[.6rem] fill-none" viewBox="0 0 100 100">
+//                     <motion.path
+//                         d="M 20,35 50,65 80,35"
+//                         animate={api}
+//                         variants={variants}
+//                         transition={{ ease: "easeInOut", duration: .2, }}
+//                     />
+//                 </svg>
+//             </div>
+//         </div>
+//     );
+// }
